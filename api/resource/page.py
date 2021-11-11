d9187459aa9ac2d1a5b4dfbc6f0ebc36774fe1d6
shooterhex <1937891400@qq.com>
2021-11-11 21:58:16 +0800

    delete recycle

--- api/resource/page.py
+++ api/resource/page.py
@@ -19,24 +19,7 @@ class GetIndex(Resource):
 
     def post(self):
         return self.get()
-
-
-class GetRecycleBin(Resource):
-    """回收站
-    """
-    def get(self):
-        pic_li = os.listdir('./static/img/recycle/')
-        pic_li.sort(reverse=True)
-        a = datetime.datetime.now()
-        a = a + datetime.timedelta(0.5)
-        time_now = datetime.datetime.strftime(a, "%Y-%m-%d")
-        context = {'name': "xiaohua", 'li': pic_li, "time": time_now}
-        return make_response(render_template('recycle.html', context=context))
-
-    def post(self):
-        return self.get()
-
-
+        
 class GetDetail(Resource):
     """详情页
     """
