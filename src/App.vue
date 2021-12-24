20dd3fdfcc6c9bf87c2478fe694a256884f41a09
shooterhex <1937891400@qq.com>
2021-12-24 21:39:36 +0800

    忘记用git add了... x2

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <demo></demo>
+  </div>
+</template>
+
+<script>
+import Demo from './components/Demo.vue'
+export default {
+  name: 'App',
+  components:{Demo}
+}
+</script>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
